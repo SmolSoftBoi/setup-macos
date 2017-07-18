f682e2476bf29a88a2a1bd1a254bee444b49fe14
Kristian Matthews <kristian.matthews@me.com>
2017-07-18 20:17:48 +0100

    Fix Computer Name
    Remove `’` around computer name.

--- setup.sh
+++ setup.sh
@@ -70,9 +70,9 @@ step "Setting your computer name."
 echo -ne "  What would you like it to be? ${BOLD}"
 read computer_name
 echo -ne "${RESET}"
-sudo scutil --set ComputerName "'${computer_name}'"
-sudo scutil --set HostName "'${computer_name}'"
-sudo defaults write /Library/Preferences/SystemConfiguration/com.apple.smb.server NetBIOSName -string "'${computer_name}'"
+sudo scutil --set ComputerName "${computer_name}"
+sudo scutil --set HostName "${computer_name}"
+sudo defaults write /Library/Preferences/SystemConfiguration/com.apple.smb.server NetBIOSName -string "${computer_name}"
 
 chapter "Adjusting Mouse & Trackpad Settings"
 
